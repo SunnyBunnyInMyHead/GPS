7930f256d4eefdf7bcacf770b8bed5c27417f1da
yevhenii.skliarov <yevhenii.skliarov@logika.tech>
2019-05-07 01:59:12 +0300

    add port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        SerialPort port = new SerialPort("/dev/ttyAMA0");
+        if(!port.isOpened()) {
+            try {
+                port.openPort();
+                int i = 0;
+                while (i<100){
+                    System.out.println(port.getInputBufferBytesCount());
+                    i++;
+                }
+            } catch (SerialPortException e) {
+                e.printStackTrace();
+            }finally {
+                try {
+                    port.closePort();
+                } catch (SerialPortException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
     }
 }
